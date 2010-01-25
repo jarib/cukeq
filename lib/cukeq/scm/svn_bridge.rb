d18e7dd866f5eb55b147f5deaac85387ad64aab8
Jari Bakken <jari.bakken@gmail.com>
2010-01-25 20:28:55 +0100

    More SvnBridge work - simple auth + insecure ssl handling

--- lib/cukeq/scm/svn_bridge.rb
+++ lib/cukeq/scm/svn_bridge.rb
@@ -23,6 +23,18 @@ module CukeQ
         @client ||= begin
           ctx = Svn::Client::Context.new
 
+          ctx.add_simple_prompt_provider(0) do |cred, realm, username, save|
+            cred.username = ENV['CUKEQ_SVN_USERNAME'] || @url.user
+            cred.password = ENV['CUKEQ_SVN_PASSWORD'] || @url.password
+            cred
+          end
+
+          if ENV['CUKEQ_SVN_INSECURE_SSL']
+            ctx.add_ssl_server_trust_prompt_provider do |cred, host, failures, info, was|
+              cred.accepted_failures = failures
+              cred
+            end
+          end
 
           unless File.directory? @working_copy
             log self.class, :checkout, @url.to_s => @working_copy
