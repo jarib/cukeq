17fa9f6947ac452e183551e2b4f60da354729ad6
Jari Bakken <jari.bakken@gmail.com>
2010-01-27 18:30:15 +0100

    SvnBridge: Read simple auth from config dir. Needs cleanup.

--- lib/cukeq/scm/svn_bridge.rb
+++ lib/cukeq/scm/svn_bridge.rb
@@ -7,41 +7,74 @@ module CukeQ
       def initialize(url, working_copy)
         @url          = url
         @working_copy = working_copy
+
+        setup_auth
       end
 
       def update
-        client.update(@working_copy).to_s
+        ensure_working_copy
+        ctx.update(@working_copy).to_s
       end
 
       def current_revision
-        client.status(@working_copy).to_s
+        ensure_working_copy
+        ctx.status(@working_copy).to_s
       end
 
       private
 
-      def client
-        @client ||= begin
-          ctx = Svn::Client::Context.new
+      def ctx
+        @ctx ||= Svn::Client::Context.new
+      end
 
-          ctx.add_simple_prompt_provider(0) do |cred, realm, username, save|
-            cred.username = ENV['CUKEQ_SVN_USERNAME'] || @url.user
-            cred.password = ENV['CUKEQ_SVN_PASSWORD'] || @url.password
+      def setup_auth
+        ctx.add_simple_prompt_provider(0) do |cred, realm, username, save|
+          cred.username = find_username(realm)
+          cred.password = find_password(realm)
+          cred
+        end
+
+        if ENV['CUKEQ_SVN_INSECURE_SSL']
+          ctx.add_ssl_server_trust_prompt_provider do |cred, host, failures, info, was|
+            cred.accepted_failures = failures
             cred
           end
+        end
+      end
 
-          if ENV['CUKEQ_SVN_INSECURE_SSL']
-            ctx.add_ssl_server_trust_prompt_provider do |cred, host, failures, info, was|
-              cred.accepted_failures = failures
-              cred
-            end
-          end
+      def find_username(realm)
+        user = ENV['CUKEQ_SVN_USERNAME'] || @url.user
+        return user if user
 
-          unless File.directory? @working_copy
-            log self.class, :checkout, @url.to_s => @working_copy
-            ctx.checkout(@url.to_s, @working_copy)
-          end
+        auth = Svn::Core::Config.read_auth_data(Svn::Core::AUTH_CRED_SIMPLE, realm)
+        raise_auth_error unless auth
 
-          ctx
+        auth["username"]
+      end
+
+      def find_password(realm)
+        pass = ENV['CUKEQ_SVN_PASSWORD'] || @url.password
+        return pass if pass
+
+        auth = Svn::Core::Config.read_auth_data(Svn::Core::AUTH_CRED_SIMPLE, realm)
+        raise_auth_error unless auth
+
+        auth["password"]
+      end
+
+      def raise_auth_error
+        raise <<-END
+         no svn authorization provided. try:
+            * set CUKEQ_SVN_USERNAME and CUKEQ_SVN_PASSWORD
+            * add username and password to the repo URL: https://foo:bar@svn.example.com/
+            * make sure correct simple auth is provided in ~/.subversion
+         END
+      end
+
+      def ensure_working_copy
+        unless File.directory? @working_copy
+          log self.class, :checkout, @url.to_s => @working_copy
+          ctx.checkout(@url.to_s, @working_copy)
         end
       end
 
